ac5fcf26b1de78fde1dc54e61fb5ccad499c4ae8
Arnold-O <arnoldoghiator@gmail.com>
2022-07-03 08:15:30 +0100

    uploaded files

--- app.js
+++ app.js
@@ -1,26 +1,19 @@
-const express = require('express')
+const express = require("express");
 
-const app = express()
-const fileupload = require('express-fileupload')
+const app = express();
+const fileupload = require("express-fileupload");
 
-app.use(express.static('./public'))
-app.use(express.json())
-app.use(fileupload())
-
+app.use(express.static("./public"));
+app.use(express.json());
+app.use(fileupload());
 
 // routes
-const ProductRoutes = require('./route/productRoute')
+const ProductRoutes = require("./route/productRoute");
 
+app.get("/", (req, res) => {
+  res.send("here we go");
+});
 
-app.get('/', (req, res)=>{
+app.use("/api/v1/products", ProductRoutes);
 
-    res.send('here we go')
-})
-
-
-app.use('/api/v1/products', ProductRoutes)
-
-
-
-
-module.exports = app
+module.exports = app;
--- controller/productControllers.js
+++ controller/productControllers.js
@@ -2,18 +2,27 @@ const catchAsyncWrapper = require("../middleware/catchAsyncWrapper");
 const Product = require("../model/productModel");
 
 exports.createProduct = catchAsyncWrapper(async (req, res, next) => {
+  const { name, price, image } = req.body;
 
-    const {name, price, image} = req.body
-
-    const newProduct = await Product.create({
-        name,
-        price,
-        image
-
-    })
+  const newProduct = await Product.create({
+    name,
+    price,
+    image,
+  });
   res.status(200).json({
     message: "create product route",
+    newProduct
   });
 });
 
-exports.getAllProduct = catchAsyncWrapper(async (req, res, next) => {});
+exports.getAllProduct = catchAsyncWrapper(async (req, res, next) => {
+
+  const getProducts = await Product.find({})
+
+  res.status(200).json({
+    success:true,
+    getProducts
+  })
+
+
+});
